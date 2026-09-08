abe4814d82eebe2c7eedcc48d513a2ede7d021de
Maryellie26 <mariellesilang2005@gmail.com>
2026-09-08 16:41:08 +0800

    updated

--- admin_history.php
+++ admin_history.php
@@ -149,7 +149,7 @@ if ($res) {
             display: flex;
             justify-content: center;
             align-items: flex-start;
-            background-image: url('images/cheesecakebg.png');
+            background-image: url('images/cheesecakebgg.png');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
